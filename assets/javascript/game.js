590d00d264e124070b8f622483e44d945a1393a2
ejpelagio <ejpelagio@yahoo.com>
2016-11-27 00:53:40 -0600

    html update

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Letting .js know to run 
+$(document).ready(function() {
+
+	var characters = {
+    "Tazmania Devil": {
+      name: "Tazmania Devil",
+      health: 120,
+      attack: 8,
+      imageUrl: "assets/images/taz.jpg",
+      enemyAttackBack: 15
+        }
+
+
+}
